f4b7d219e9b3d7fcc57d37486e6b054b97b72c68
Mar71n1 <morzeszko98@gmail.com>
2019-11-30 22:45:45 +0100

    First commit

---
+++ app/src/main/java/pl/pwr/swipeanimation/MainActivity.java
@@ -0,0 +1,17 @@
+package pl.pwr.swipeanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+}
